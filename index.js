bccaceeb85c98a4de684a0b8e033da9b113ee56b
hummingbee57 <bhasinmaya01@gmail.com>
2026-02-20 08:28:42 -0500

    added .env

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB31mLa3AvjMSUGmEcpLKbcIOJTee-Seq8",
+  apiKey: process.env.FIREBASE_APIKEY,
   authDomain: "tsa-webmaster-1.firebaseapp.com",
   databaseURL: "https://tsa-webmaster-1-default-rtdb.firebaseio.com",
   projectId: "tsa-webmaster-1",
   storageBucket: "tsa-webmaster-1.firebasestorage.app",
   messagingSenderId: "344004193042",
-  appId: "1:344004193042:web:55ae4f5b54b7f085a461cb"
+  appId: process.env.FIREBASE_APPID
 };
 
 // Initialize Firebase
